b58d193a2d26a4b3a8f1aa9bc663732e417694d7
Dannylo Carlos <lorddancarlos@gmail.com>
2017-08-04 14:07:25 -0300

    First

---
+++ XamarinAds/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Ads;
+
+namespace XamarinAds{
+    [Activity(Label = "XamarinAds", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity{
+
+        protected AdView mAdView;
+        protected InterstitialAd mInterstitialAd;
+        protected Button mLoadInterstitialButton;
+
+        protected override void OnCreate(Bundle bundle){
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+            //-------------------------------
+            var appid = "ca-app-pub-1511238554569737~3292095533"; //Id do APP
+            MobileAds.Initialize(ApplicationContext, appid);
+
+            var adview = FindViewById<AdView>(Resource.Id.adView);
+            var adrequest = new AdRequest.Builder().Build();
+            adview.LoadAd(adrequest);
+            
+
+
+        }
+    }
+}
+
